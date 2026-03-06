d1978db3c04c94e43d72b631931f4d82ce2c0ffd
keneshbekovanvar-ship-it <keneshbekovanvar@mail.ru>
2026-03-06 21:40:13 +0600

    hw3

--- product/urls.py
+++ product/urls.py
@@ -10,12 +10,11 @@ urlpatterns = [
     path('categories/', CategoryListView.as_view()),
     path('categories/<int:pk>/', CategoryDetailView.as_view()),
 
-
-    path('products/reviews/', ProductReviewListView.as_view()),
-
     path('products/', ProductListView.as_view()),
     path('products/<int:pk>/', ProductDetailView.as_view()),
 
+
     path('reviews/', ReviewListView.as_view()),
     path('reviews/<int:pk>/', ReviewDetailView.as_view()),
+    path('products/<int:product_id>/reviews/', ProductReviewListView.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -1,31 +1,42 @@
 from rest_framework import generics
 from .models import Category, Product, Review
-from .serializers import CategorySerializer, ProductSerializer, ReviewSerializer, ProductReviewSerializer
+from .serializers import CategorySerializer, ProductSerializer, ReviewSerializer
+
+
+class CategoryListView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductListView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+
+class ReviewListView(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
 
 class ProductReviewListView(generics.ListAPIView):
-    queryset = Review.objects.all()
-    serializer_class = ProductReviewSerializer
-
-class CategoryListView(generics.ListAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-
-class CategoryDetailView(generics.RetrieveAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-
-class ProductListView(generics.ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-class ProductDetailView(generics.RetrieveAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-class ReviewListView(generics.ListAPIView):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
-class ReviewDetailView(generics.RetrieveAPIView):
-    queryset = Review.objects.all()
-    serializer_class = ReviewSerializer
+    def get_queryset(self):
+        product_id = self.kwargs.get('product_id')  # из URL
+        return Review.objects.filter(product_id=product_id)
