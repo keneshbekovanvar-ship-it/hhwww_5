0a106a35c6bdb2516ec968c113577f6c6092a141
keneshbekovanvar-ship-it <keneshbekovanvar@mail.ru>
2026-03-09 22:27:20 +0600

    hw4

--- product/serializers.py
+++ product/serializers.py
@@ -2,24 +2,48 @@ from rest_framework import serializers
 from .models import Category, Product, Review
 
 
+
 class CategorySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Category
         fields = '__all__'
 
+    def validate_name(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Category name must be at least 3 characters.")
+        return value
+
 
 class ProductSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Product
         fields = '__all__'
 
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Price must be greater than 0.")
+        return value
+
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Product name is too short.")
+        return value
+
 
 class ReviewSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Review
         fields = '__all__'
 
-class ProductReviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
-        fields = 'all'
+    def validate_rating(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("Rating must be between 1 and 5.")
+        return value
+
+    def validate_text(self, value):
+        if len(value) < 5:
+            raise serializers.ValidationError("Review text is too short.")
+        return value
