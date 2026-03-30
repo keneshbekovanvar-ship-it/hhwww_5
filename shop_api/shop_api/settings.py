783e1699b4564046cd7294a3daf9a82206a369a2
keneshbekovanvar-ship-it <keneshbekovanvar@mail.ru>
2026-03-31 00:06:44 +0600

    hw8

--- shop_api/shop_api/settings.py
+++ shop_api/shop_api/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'NAME': 'shop_db',
         'USER': 'postgres',
         'PASSWORD': '1234',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
