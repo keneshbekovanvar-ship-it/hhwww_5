d5a4ae0a70266e2b9ddb5eb93a124b5bcb86b204
keneshbekovanvar-ship-it <keneshbekovanvar@mail.ru>
2026-04-03 22:55:53 +0600

    hw1

--- shop_api/product/models.py
+++ shop_api/product/models.py
@@ -1,36 +1,11 @@
-from django.contrib.auth.models import User
 from django.db import models
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
-class Review(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    text = models.TextField()
-    rating = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.product.name} - {self.rating}"
-
-
+from django.conf import settings
 
 class ConfirmationCode(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
-        return f"{self.user.username} - {self.code}"
+        return f"Code {self.code} for {self.user.username}"
---
+++ users/views.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from .models import CustomUser
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'password', 'phone_number']
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(
+            email=data['email'],
+            password=data['password']
+        )
+        if not user:
+            raise serializers.ValidationError("Неверные данные")
+        return user
